f18c777710f43547da57abf4d5e2da017d0b2af4
Federico Naveira <federico.naveira@klarna.com>
2021-11-12 11:18:36 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getDepartures } from './services/sl'
+
+const STOP_AREA_NAME = 'AGA'
+const DESTINATION = 'Ropsten'
+const PULL_DATA_INTERVAL = 5000
+const MIN_TIME_BEFORE_LEAVING = 6 * 60 * 1000
+const WARNING_TIME_GAP = 3 * 60 * 1000
+
+interface SLDepartureType {
+  stopAreaName: string,
+  destination: string,
+  time: {
+    displayTime: string,
+    expectedDateTime: string
+  }
+}
+
+const getTimeToLeaveStatus = (departure : SLDepartureType) : string => {
+  const timeArrival : Date = new Date(departure.time.expectedDateTime)
+  const diff = timeArrival.getTime() - new Date().getTime()
+  console.log(timeArrival, diff, MIN_TIME_BEFORE_LEAVING)
+  if (diff - MIN_TIME_BEFORE_LEAVING < 0) {
+    return 'run-now'
+  } else if (diff - MIN_TIME_BEFORE_LEAVING - WARNING_TIME_GAP < 0) {
+    return 'warning'
+  } else {
+    return 'good'
+  }
+}
 
 function App() {
+  const [remainingTime, setRemainingTime] = useState([])
+  const [activated, setActivated] = useState(false)
+
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      getDepartures().then((resp) => {
+        resp.filter((item : SLDepartureType) => {
+          return item.stopAreaName === STOP_AREA_NAME && item.destination === DESTINATION
+        })
+        setRemainingTime(resp)
+      })
+    }, PULL_DATA_INTERVAL)
+    return () => {
+      clearInterval(intervalRef)
+    }
+  }, [])
+  console.log(remainingTime)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+        </div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {remainingTime.map((departure: SLDepartureType, index: number) => {
+            return (
+              <span style={{ margin: '4px' }} className={`departure-${index} ${getTimeToLeaveStatus(departure)}`}>
+                {departure.time.displayTime}
+              </span>
+            )
+          })}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/services/sl.ts
@@ -0,0 +1,20 @@
+export const getDepartures = () => {
+  return fetch('http://localhost:8010/proxy/api/v2/departures?mode=departures&origPlaceId=QT0xQE89QWdhIChMaWRpbmfDtilAWD0xODE1NDU3OUBZPTU5MzQ2ODY1QFU9NzRATD0zMDAxMDkyNDhAQj0xQHA9MTYzNTk5MTIxMUA%3D&origSiteId=9248&desiredResults=3&origName=Aga+%28Liding%C3%B6%29', {
+    'headers': {
+      'accept': '*/*',
+      'accept-language': 'en-US',
+      'cache-control': 'no-cache',
+      'pragma': 'no-cache',
+      'sec-ch-ua-mobile': '?0',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site'
+    },
+    'referrer': 'https://sl.se/',
+    'referrerPolicy': 'origin-when-cross-origin',
+    'body': null,
+    'method': 'GET',
+    'mode': 'cors',
+    'credentials': 'omit'
+  }).then(resp => resp.json())
+}
+

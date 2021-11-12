46515a1fdf6f640c9a8ccc268aa7fd5e87a660fc
Federico Naveira <federico.naveira@klarna.com>
2021-11-12 15:59:24 +0100

    added proxy call

--- src/services/sl.ts
+++ src/services/sl.ts
@@ -1,5 +1,6 @@
 export const getDepartures = () => {
-  return fetch('https://cors-anywhere.herokuapp.com/https://webcloud.sl.se/api/v2/departures?mode=departures&origPlaceId=QT0xQE89QWdhIChMaWRpbmfDtilAWD0xODE1NDU3OUBZPTU5MzQ2ODY1QFU9NzRATD0zMDAxMDkyNDhAQj0xQHA9MTYzNTk5MTIxMUA%3D&origSiteId=9248&desiredResults=3&origName=Aga+%28Liding%C3%B6%29', {
+  // return fetch('https://cors-anywhere.herokuapp.com/https://webcloud.sl.se/api/v2/departures?mode=departures&origPlaceId=QT0xQE89QWdhIChMaWRpbmfDtilAWD0xODE1NDU3OUBZPTU5MzQ2ODY1QFU9NzRATD0zMDAxMDkyNDhAQj0xQHA9MTYzNTk5MTIxMUA%3D&origSiteId=9248&desiredResults=3&origName=Aga+%28Liding%C3%B6%29', {
+    return fetch('/api/sl', {
     'headers': {
       'accept': '*/*',
       'accept-language': 'en-US',

3ae12703c326600a112ca1d2c80a3e10f34be178
Federico Naveira <federico.naveira@klarna.com>
2021-11-17 16:47:40 +0100

    Added timeout to service worker

--- src/index.tsx
+++ src/index.tsx
@@ -27,19 +27,21 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // @ts-ignore
       window.swRegistration = registration
-      registration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(
-          'BPQ31iS_wnpweqM4eAu2BtoYuphbyNXA8here-MlI5RiFCdAt9E6gVhaCtngo_zuAP-xBB2T0C5dO5iBTpbNCbc' // PUBLIC KEY
-        )
-      }).then(function(pushSubscription) {
-        console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-        // Send subscription to server
-        sendSubscriptionToBackend(pushSubscription)
-        return pushSubscription;
-      });
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ' + registration.scope);
+      setTimeout(() => {
+        registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(
+            'BPQ31iS_wnpweqM4eAu2BtoYuphbyNXA8here-MlI5RiFCdAt9E6gVhaCtngo_zuAP-xBB2T0C5dO5iBTpbNCbc' // PUBLIC KEY
+          )
+        }).then(function(pushSubscription) {
+          console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+          // Send subscription to server
+          sendSubscriptionToBackend(pushSubscription)
+          return pushSubscription;
+        });
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ' + registration.scope);
+      }, 3000)
     }, function(err) {
       // registration failed :(
       alert('ServiceWorker registration failed: ' + err.message);
